0956de28884b5f2fbfe822304ae30768a4dd6bc0
Ewan Hemingway <ewan.hemingway@gmail.com>
2011-08-23 17:06:27 +0100

    Changed to slower, array based control

--- dla.py
+++ dla.py
@@ -5,7 +5,7 @@ from math import pi, sqrt, cos, sin
 from random import choice, random, seed
 
 from pylab import pcolormesh, axes, show
-from numpy import zeros, int32, arange
+from numpy import array, zeros, int32, arange
 
 twopi = 2 * pi              # 2 pi for choosing starting position
 
@@ -13,8 +13,8 @@ hits = 0                    # how many seeds have stuck
 birthradius = 5             # starting radius for walkers
 deathradius = 10            # radius to kill off walkers
 maxradius = -1              # the extent of the growth
-numParticles = 10000        # how many walkers to release
-#seed(42)                    # seed for debugging
+numParticles = 1000         # how many walkers to release
+seed(42)                    # seed for debugging
 
 L = 500                     # lattice goes from -L : L
 size = (2 * L) + 1          # so total lattice width is 2L + 1
@@ -25,17 +25,19 @@ lattice[L, L] = -1
 
 # possible (relative) nearest neighbour sites
 nnsteps = ((0, 1), (0, -1), (1, 0), (-1, 0))
+# account for lattice zero not being array zero
+nnstepsLattice = array(nnsteps) + L
 
 
 # returns whether site pos = (x, y) has
 # an occupied nearest-neighbour site
 def nnOccupied(pos):
 
-    # convert from lattice coords to array coords
-    latticepos = (pos[0] + L, pos[1] + L)
-
-    for step in nnsteps:
-        if lattice[latticepos[0] + step[0], latticepos[1] + step[1]] != 0:
+    # find neighbouring sites on lattice
+    neighbours = nnstepsLattice + pos
+    #print neighbours
+    for nx, ny in neighbours:
+        if lattice[nx, ny] != 0:
             return True
     else:
         return False
@@ -45,7 +47,8 @@ def nnOccupied(pos):
 # check if a point is within the
 # allowed radius
 def inCircle(pos):
-    if (pos[0] ** 2 + pos[1] ** 2) > deathradius ** 2:  # faster than sqrt
+    #print "circ", pos, pos[0], deathradius
+    if sum(pos ** 2) > deathradius ** 2:  # faster than sqrt
         return False
     else:
         return True
@@ -57,14 +60,15 @@ def registerHit(pos):
     global hits, birthradius, deathradius, maxradius
 
     # check if this "hit" extends the max radius
-    norm2 = (pos[0] ** 2 + pos[1] ** 2)
+    norm2 = sum(pos ** 2)
     if norm2 > maxradius ** 2:
         maxradius = int(sqrt(norm2))
         birthradius = maxradius + 5 if (maxradius + 5) < L else L
         deathradius = maxradius + 20 if (maxradius + 20) < L else L
 
     hits += 1
-    lattice[pos[0] + L, pos[1] + L] = hits
+    lx, ly = pos + L
+    lattice[lx, ly] = hits
 # end of registerHit
 
 
@@ -79,7 +83,7 @@ for particle in xrange(numParticles):
     angle = random() * twopi
     # and convert to a starting position, pos = (x, y),
     # on a circle of radius "birthradius" around the centre seed
-    pos = [int(sin(angle) * birthradius), int(cos(angle) * birthradius)]
+    pos = array((int(sin(angle) * birthradius), int(cos(angle) * birthradius)))
 
     isDead = False      # walker starts off alive
 
@@ -88,8 +92,7 @@ for particle in xrange(numParticles):
         # pick one of the nearest neighbour sites to explore
         moveDir = choice(nnsteps)
         # and apply the selected move to position coordinate, pos
-        pos[0] += moveDir[0]
-        pos[1] += moveDir[1]
+        pos += moveDir
 
         if not inCircle(pos):
             isDead = True
