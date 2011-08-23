07dc47a454158dd7aaeb22b55b1cb6f6683d0f9d
Ewan Hemingway <ewan.hemingway@gmail.com>
2011-08-23 20:18:36 +0100

    Reworked main code into a seperate class to avoid global variables, seperated main method and plotting method into seperate functions.

--- dla.py
+++ dla.py
@@ -7,109 +7,121 @@ from random import choice, random, seed
 from pylab import pcolormesh, axes, show
 from numpy import zeros, int32, arange
 
-twopi = 2 * pi              # 2 pi for choosing starting position
 
-hits = 0                    # how many seeds have stuck
-birthradius = 5             # starting radius for walkers
-deathradius = 10            # radius to kill off walkers
-maxradius = -1              # the extent of the growth
-numParticles = 10000        # how many walkers to release
-#seed(42)                    # seed for debugging
+class dla(object):
 
-L = 500                     # lattice goes from -L : L
-size = (2 * L) + 1          # so total lattice width is 2L + 1
+    def __init__(self, numParticles, L=500):
+        self.L = L              # lattice goes from -L : L
+        size = (2 * L) + 1      # so total lattice width is 2L + 1
+        self.numParticles = numParticles    # how many walkers to release
 
-# preallocate and initialise centre point as "seed"
-lattice = zeros((size, size), dtype=int32)
-lattice[L, L] = -1
+        self.hits = 0                       # how many seeds have stuck
+        self.birthradius = 5                # starting radius for walkers
+        self.deathradius = 10               # radius to kill off walkers
+        self.maxradius = -1                 # the extent of the growth
+        seed(42)                    # seed for debugging
 
-# possible (relative) nearest neighbour sites
-nnsteps = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        # preallocate and initialise centre point as "seed"
+        self.lattice = zeros((size, size), dtype=int32)
+        self.lattice[L, L] = -1
 
+        # possible (relative) nearest neighbour sites
+        self.nnsteps = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        # possible (absolute) nearest neighbour sites
+        self.nnstepsLattice = ((L, L + 1), (L, L - 1), (L + 1, L), (L - 1, L))
 
-# returns whether site pos = (x, y) has
-# an occupied nearest-neighbour site
-def nnOccupied(pos):
+    # returns whether site pos = (x, y) has
+    # an occupied nearest-neighbour site
+    def nnOccupied(self, pos):
 
-    # convert from lattice coords to array coords
-    latticepos = (pos[0] + L, pos[1] + L)
+        # convert from lattice coords to array coords
+        for step in self.nnstepsLattice:
+            if self.lattice[pos[0] + step[0], pos[1] + step[1]] != 0:
+                return True
+        else:
+            return False
+    # end of nnOccupied
 
-    for step in nnsteps:
-        if lattice[latticepos[0] + step[0], latticepos[1] + step[1]] != 0:
+    # check if a point is within the
+    # allowed radius
+    def inCircle(self, pos):
+        # faster than sqrt
+        if (pos[0] ** 2 + pos[1] ** 2) > self.deathradius ** 2:
+            return False
+        else:
             return True
-    else:
-        return False
-# end of nnOccupied
+    # end of inCircle
 
+    # registers an extension on the seed
+    def registerHit(self, pos):
 
-# check if a point is within the
-# allowed radius
-def inCircle(pos):
-    if (pos[0] ** 2 + pos[1] ** 2) > deathradius ** 2:  # faster than sqrt
-        return False
-    else:
-        return True
-# end of inCircle
+        # check if this "hit" extends the max radius
+        norm2 = (pos[0] ** 2 + pos[1] ** 2)
+        if norm2 > self.maxradius ** 2:
+            self.maxradius = int(sqrt(norm2))
 
+            self.birthradius = self.maxradius + 5 \
+                if (self.maxradius + 5) < self.L else self.L
+            self.deathradius = self.maxradius + 20 \
+                if (self.maxradius + 20) < self.L else self.L
 
-# registers an extension on the seed
-def registerHit(pos):
-    global hits, birthradius, deathradius, maxradius
+        self.hits += 1
+        self.lattice[pos[0] + self.L, pos[1] + self.L] = self.hits
+    # end of registerHit
 
-    # check if this "hit" extends the max radius
-    norm2 = (pos[0] ** 2 + pos[1] ** 2)
-    if norm2 > maxradius ** 2:
-        maxradius = int(sqrt(norm2))
-        birthradius = maxradius + 5 if (maxradius + 5) < L else L
-        deathradius = maxradius + 20 if (maxradius + 20) < L else L
+    # main logic loop
+    def main(self):
 
-    hits += 1
-    lattice[pos[0] + L, pos[1] + L] = hits
-# end of registerHit
+        starttime = time.time()
+        print "Running", self.numParticles, "particles..."
 
+        # 2*pi for choosing starting position
+        twopi = 2 * pi
+        for particle in xrange(self.numParticles):
 
-starttime = time.time()
-print "Running", numParticles, "particles..."
+            # find angle on [0, 2pi)
+            angle = random() * twopi
+            # and convert to a starting position, pos = (x, y),
+            # on a circle of radius "birthradius" around the centre seed
+            pos = [int(sin(angle) * self.birthradius), \
+                int(cos(angle) * self.birthradius)]
 
-for particle in xrange(numParticles):
+            isDead = False      # walker starts off alive
 
-    #print particle
+            while not isDead:
 
-    # find angle on [0, 2pi)
-    angle = random() * twopi
-    # and convert to a starting position, pos = (x, y),
-    # on a circle of radius "birthradius" around the centre seed
-    pos = [int(sin(angle) * birthradius), int(cos(angle) * birthradius)]
+                # pick one of the nearest neighbour sites to explore
+                moveDir = choice(self.nnsteps)
+                # and apply the selected move to position coordinate, pos
+                pos[0] += moveDir[0]
+                pos[1] += moveDir[1]
 
-    isDead = False      # walker starts off alive
+                if not self.inCircle(pos):
+                    isDead = True
+                    break
+                elif self.nnOccupied(pos):
+                    self.registerHit(pos)
+                    isDead = True
+                    break
+        endtime = time.time()
+        print "Ran in time:", (endtime - starttime)
+        print "Maximum radius:", self.maxradius
 
-    while not isDead:
+    # use matplotlib to plot a heat map
+    # hotter colours mean newer parts of the fractal
+    def plotLattice(self):
 
-        # pick one of the nearest neighbour sites to explore
-        moveDir = choice(nnsteps)
-        # and apply the selected move to position coordinate, pos
-        pos[0] += moveDir[0]
-        pos[1] += moveDir[1]
+        # select only the interesting parts
+        M = self.maxradius
+        grph = self.L - M, self.L + M
 
-        if not inCircle(pos):
-            isDead = True
-            break
-        elif nnOccupied(pos):
-            registerHit(pos)
-            isDead = True
-            break
+        # and plot
+        axis = arange(-M, M + 1)
+        pcolormesh(axis, axis, self.lattice[grph[0]:grph[1], grph[0]:grph[1]])
+        axes().set_aspect('equal', 'datalim')
+        show()
 
-
-endtime = time.time()
-print "Ran in time:", (endtime - starttime)
-print "Maximum radius:", maxradius
-
-# select only the interesting parts
-M = maxradius
-grph = L - M, L + M
-
-# and plot
-axis = arange(-M, M + 1)
-pcolormesh(axis, axis, lattice[grph[0]:grph[1], grph[0]:grph[1]])
-axes().set_aspect('equal', 'datalim')
-show()
+# run the main program
+model = dla(5000)
+model.main()
+model.plotLattice()
